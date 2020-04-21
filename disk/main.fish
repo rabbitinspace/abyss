2454103f77bcc32912672a1b167b06a28989c1c8
Igor Nikitin <rabbitinspace@icloud.com>
2020-04-21 21:19:49 +1000

    more logs

--- disk/main.fish
+++ disk/main.fish
@@ -1,4 +1,4 @@
-set ROOT (git rev-parse --show-toplevel || pwd)
+set ROOT (type -q git && git rev-parse --show-toplevel || pwd)
 set DIR (dirname (status --current-filename))
 
 source "$ROOT/common/log.fish"
@@ -10,11 +10,13 @@ source "$DIR/partition.fish"
 
 # Picks a disk and prepares it for base system installation.
 function prepare_disk
+  log_info "Detecting disk for installation."
   if ! set -l disk (detect_disk)
     log_err "Failed to detect disk to use."
     return 1
   end
 
+  log_info "Partitioning $disk."
   if ! partition $disk
     log_err "Failed to partition $disk."
     return 1
@@ -23,26 +25,31 @@ function prepare_disk
   set -l efi (get_partition $disk 1)
   set -l root (get_partition $disk 2)
 
+  log_info "Encrypting $root."
   if ! encrypt $root $LUKS_PASSWORD
     log_err "Failed to create LUKS partition: $root."
     return 1
   end
 
+  log_info "Attaching encrypted $root."
   if ! set -l mapper (attach $root $LUKS_LABEL $LUKS_PASSWORD)
     log_err "Failed to attach LUKS partition: $root."
     return 1
   end
 
+  log_info "Formatting boot partition: $efi."
   if ! mkefi $efi
     log_err "Failed to create boot partition: $efi."
     return 1
   end
 
+  log_info "Formatting root partition: $mapper."
   if ! mkbtrfs $mapper $MOUNT_OPTS $BTRFS_SUBVOLS
     log_err "Failed to create root partition: $mapper."
     return 1
   end
 
+  log_info "Mounting all partitions for system installation."
   if ! mount_parts $efi $mapper $BTRFS_SUBVOLS $MOUNT_OPTS
     log_err "Failed to mount partition for system installation."
     return 1
